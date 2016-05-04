0871d1e64bc846a97879dcb1f95c5ff152d203e1
Eric Emmons <eric.emmons@gmail.com>
2016-05-04 17:07:55 -0400

    fonts and images check

--- lowfid/js/app.js
+++ lowfid/js/app.js
@@ -1,1 +1,14 @@
 $(document).foundation()
+
+$("i").click(function() {
+
+    var favoriteCheck = $(this).data("add-favorite");
+    var favoriteStatus = $(this).hasClass('fa-heart-o');
+    if (favoriteCheck && favoriteStatus) {
+        $(this).removeClass('fa-heart-o');
+        $(this).addClass('fa-heart');
+    } else if (favoriteCheck && !favoriteStatus) {
+        $(this).removeClass('fa-heart');
+        $(this).addClass('fa-heart-o');
+    }
+});
